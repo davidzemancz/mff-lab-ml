a6f16d3ce95484e6aef33613244f483c5c29f67a
David Zeman <davca.zeman@gmail.com>
2021-10-30 12:40:36 +0200

    Logistic regression SGD

--- labs/03/logistic_regression_sgd.py
+++ labs/03/logistic_regression_sgd.py
@@ -1,5 +1,9 @@
+# fbc0b6cc-0238-11eb-9574-ea7484399335
+# 7b885094-03f8-11eb-9574-ea7484399335
+
 import argparse
 import sys
+import math
 
 import numpy as np
 import sklearn.datasets
@@ -10,13 +14,17 @@ parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
 parser.add_argument("--batch_size", default=10, type=int, help="Batch size")
 parser.add_argument("--data_size", default=100, type=int, help="Data size")
-parser.add_argument("--iterations", default=50, type=int, help="Number of iterations over the data")
+parser.add_argument("--epochs", default=50, type=int, help="Number of epochs over the data")
 parser.add_argument("--learning_rate", default=0.01, type=float, help="Learning rate")
 parser.add_argument("--plot", default=False, const=True, nargs="?", type=str, help="Plot the predictions")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
 parser.add_argument("--test_size", default=0.5, type=lambda x:int(x) if x.isdigit() else float(x), help="Test set size")
 # If you add more arguments, ReCodEx will keep them with your default values.
+
+# Sigmoid function
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
 
 def main(args: argparse.Namespace) -> tuple[np.ndarray, list[tuple[float, float]]]:
     # Create a random generator with a given seed
@@ -26,20 +34,20 @@ def main(args: argparse.Namespace) -> tuple[np.ndarray, list[tuple[float, float]
     data, target = sklearn.datasets.make_classification(n_samples=args.data_size, n_features=2, n_informative=2, n_redundant=0, random_state=args.seed)
 
     # Append a constant feature with value 1 to the end of every input data
-    dataset.data = np.append(dataset.data, np.ones([1,dataset.data.shape[0]]).transpose(), axis=1)
+    data = np.append(data, np.ones([1,data.shape[0]]).transpose(), axis=1)
 
     # Split the dataset into a train set and a test set.
     # Use `sklearn.model_selection.train_test_split` method call, passing
     # arguments `test_size=args.test_size, random_state=args.seed`.
-    train_data, test_data, train_target, test_target = sklearn.model_selection.train_test_split(dataset.data, dataset.target, test_size=args.test_size, random_state=args.seed)
+    train_data, test_data, train_target, test_target = sklearn.model_selection.train_test_split(data, target, test_size=args.test_size, random_state=args.seed)
 
     # Generate initial linear regression weights
     weights = generator.uniform(size=train_data.shape[1], low=-0.1, high=0.1)
 
-    for iteration in range(args.iterations):
+    for epoch in range(args.epochs):
         permutation = generator.permutation(train_data.shape[0])
 
-        # TODO: Process the data in the order of `permutation`. For every
+        # Process the data in the order of `permutation`. For every
         # `args.batch_size` of them, average their gradient, and update the weights.
         # You can assume that `args.batch_size` exactly divides `train_data.shape[0]`.
         # Same as linear_regression_sgd.py in 02
@@ -50,26 +58,40 @@ def main(args: argparse.Namespace) -> tuple[np.ndarray, list[tuple[float, float]
                 p = permutation[i]
                 x_i = train_data[p]
                 t_i = train_target[p]
-                gradient = ((x_i.transpose() @ weights) - t_i) * x_i
+                gradient = (sigmoid((x_i.transpose() @ weights)) - t_i) * x_i
                 gradient_sum = gradient_sum + gradient
             
             gradient_avrg = gradient_sum / args.batch_size
             weights = weights - (args.learning_rate * gradient_avrg)
 
-        # TODO: After the SGD iteration, measure the average loss and accuracy for both the
+        # TODO: After the SGD epoch, measure the average loss and accuracy for both the
         # train test and the test set. The loss is the average MLE loss (i.e., the
         # negative log likelihood, or crossentropy loss, or KL loss) per example.
-        train_accuracy, train_loss, test_accuracy, test_loss = None, None, None, None
+        train_accuracy, train_loss, test_accuracy, test_loss = 0, 0, 0, 0
+        
+        train_predictions = np.zeros(train_target.shape)
+        i = 0
+        for x_i in train_data:
+           train_predictions[i] = sigmoid((x_i.transpose() @ weights))
+           i = i + 1
+        train_loss = sklearn.metrics.log_loss(train_target, train_predictions)
+        train_accuracy = sklearn.metrics.accuracy_score(train_target, sklearn.preprocessing.binarize(train_predictions.reshape(-1, 1), threshold=0.5))
 
-        train_loss=sklearn.metrics.log_loss()
+        test_predictions = np.zeros(test_target.shape)
+        i = 0
+        for x_i in test_data:
+           test_predictions[i] = sigmoid((x_i.transpose() @ weights))
+           i = i + 1
+        test_loss = sklearn.metrics.log_loss(test_target, test_predictions)
+        test_accuracy = sklearn.metrics.accuracy_score(test_target, sklearn.preprocessing.binarize(test_predictions.reshape(-1, 1), threshold=0.5))
 
-        print("After iteration {}: train loss {:.4f} acc {:.1f}%, test loss {:.4f} acc {:.1f}%".format(iteration + 1, train_loss, 100 * train_accuracy, test_loss, 100 * test_accuracy))
+        print("After epoch {}: train loss {:.4f} acc {:.1f}%, test loss {:.4f} acc {:.1f}%".format(epoch + 1, train_loss, 100 * train_accuracy, test_loss, 100 * test_accuracy))
 
         if args.plot:
             import matplotlib.pyplot as plt
             if args.plot is not True:
-                if not iteration: plt.figure(figsize=(6.4*3, 4.8*(args.iterations+2)//3))
-                plt.subplot(3, (args.iterations+2)//3, 1 + iteration)
+                if not epoch: plt.figure(figsize=(6.4*3, 4.8*(args.epochs+2)//3))
+                plt.subplot(3, (args.epochs+2)//3, 1 + epoch)
             xs = np.linspace(np.min(data[:, 0]), np.max(data[:, 0]), 50)
             ys = np.linspace(np.min(data[:, 1]), np.max(data[:, 1]), 50)
             predictions = [[1 / (1 + np.exp(-([x, y, 1] @ weights))) for x in xs] for y in ys]
