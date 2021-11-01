e25bc2e024a6cae5d1f2ee1889519d9ef0339062
David Zeman <David.zeman@outlook.com>
2021-11-01 12:46:56 +0100

    a

--- labs/04/softmax_classification_sgd.py
+++ labs/04/softmax_classification_sgd.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import argparse
 import sys
 
@@ -11,12 +10,17 @@ parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
 parser.add_argument("--batch_size", default=10, type=int, help="Batch size")
 parser.add_argument("--classes", default=10, type=int, help="Number of classes to use")
-parser.add_argument("--iterations", default=10, type=int, help="Number of iterations over the data")
+parser.add_argument("--epochs", default=10, type=int, help="Number of epochs over the data")
 parser.add_argument("--learning_rate", default=0.01, type=float, help="Learning rate")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
 parser.add_argument("--test_size", default=797, type=lambda x:int(x) if x.isdigit() else float(x), help="Test set size")
 # If you add more arguments, ReCodEx will keep them with your default values.
+
+def softmax(x):
+    y = np.exp(x - np.max(x))
+    f_x = y / np.sum(np.exp(x))
+    return f_x
 
 def main(args: argparse.Namespace) -> tuple[np.ndarray, list[tuple[float, float]]]:
     # Create a random generator with a given seed
@@ -37,7 +41,7 @@ def main(args: argparse.Namespace) -> tuple[np.ndarray, list[tuple[float, float]
     # Generate initial model weights
     weights = generator.uniform(size=[train_data.shape[1], args.classes], low=-0.1, high=0.1)
 
-    for iteration in range(args.iterations):
+    for epoch in range(args.epochs):
         permutation = generator.permutation(train_data.shape[0])
 
         # TODO: Process the data in the order of `permutation`.
@@ -48,14 +52,43 @@ def main(args: argparse.Namespace) -> tuple[np.ndarray, list[tuple[float, float]
         # in softmax can easily overflow. To avoid it, you should use the fact that
         # softmax(z) = softmax(z + any_constant) and compute softmax(z) = softmax(z - maximum_of_z).
         # That way we only exponentiate values which are non-positive, and overflow does not occur.
+        batches_count = train_data.shape[0] / args.batch_size
+        for batch in range(1, int(batches_count) + 1):
+            gradient_sum = np.zeros([train_data.shape[1]])
+            for i in range(args.batch_size * (batch - 1), args.batch_size * batch):
+                p = permutation[i]
+                x_i = train_data[p]
+                t_i = train_target[p]
+                r = softmax(x_i.transpose() @ weights)
+                gradient = (r - t_i) * x_i
+                gradient_sum = gradient_sum + gradient
+            
+            gradient_avrg = gradient_sum / args.batch_size
+            weights = weights - (args.learning_rate * gradient_avrg)
 
-        # TODO: After the SGD iteration, measure the average loss and accuracy for both the
+        # TODO: After the SGD epoch, measure the average loss and accuracy for both the
         # train test and the test set. The loss is the average MLE loss (i.e., the
         # negative log likelihood, or crossentropy loss, or KL loss) per example.
-        train_accuracy, train_loss, test_accuracy, test_loss = None, None, None, None
+        train_accuracy, train_loss, test_accuracy, test_loss = 0, 0, 0, 0
+        
+        train_predictions = np.zeros(train_target.shape)
+        i = 0
+        for x_i in train_data:
+           train_predictions[i] = sigmoid((x_i.transpose() @ weights))
+           i = i + 1
+        train_loss = sklearn.metrics.log_loss(train_target, train_predictions)
+        #train_accuracy = sklearn.metrics.accuracy_score(train_target, sklearn.preprocessing.binarize(train_predictions.reshape(-1, 1), threshold=0.5))
 
-        print("After iteration {}: train loss {:.4f} acc {:.1f}%, test loss {:.4f} acc {:.1f}%".format(
-            iteration + 1, train_loss, 100 * train_accuracy, test_loss, 100 * test_accuracy))
+        test_predictions = np.zeros(test_target.shape)
+        i = 0
+        for x_i in test_data:
+           test_predictions[i] = sigmoid((x_i.transpose() @ weights))
+           i = i + 1
+        test_loss = sklearn.metrics.log_loss(test_target, test_predictions)
+        #test_accuracy = sklearn.metrics.accuracy_score(test_target, sklearn.preprocessing.binarize(test_predictions.reshape(-1, 1), threshold=0.5))
+
+        print("After epoch {}: train loss {:.4f} acc {:.1f}%, test loss {:.4f} acc {:.1f}%".format(
+            epoch + 1, train_loss, 100 * train_accuracy, test_loss, 100 * test_accuracy))
 
     return weights, [(train_loss, train_accuracy), (test_loss, test_accuracy)]
 
