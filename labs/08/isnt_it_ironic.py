5258afa96ad14a39901400cc596c4663c99b320c
David Zeman <davca.zeman@gmail.com>
2021-12-12 13:42:23 +0100

    isnt it ironic

--- labs/08/isnt_it_ironic.py
+++ labs/08/isnt_it_ironic.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import argparse
 import lzma
 import pickle
@@ -7,6 +6,20 @@ import urllib.request
 import sys
 
 import numpy as np
+
+import sklearn.compose
+import sklearn.datasets
+import sklearn.model_selection
+import sklearn.linear_model
+import sklearn.feature_selection
+import sklearn.metrics
+import sklearn.pipeline
+import sklearn.preprocessing
+import sklearn.neural_network
+import sklearn.kernel_approximation
+import sklearn.ensemble
+import sklearn.feature_extraction
+import sklearn.naive_bayes
 
 class Dataset:
     def __init__(self,
@@ -35,15 +48,28 @@ parser.add_argument("--recodex", default=False, action="store_true", help="Runni
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
 # For these and any other arguments you add, ReCodEx will keep your default value.
 parser.add_argument("--model_path", default="isnt_it_ironic.model", type=str, help="Model path")
+parser.add_argument("--test", default=False, type=bool, help="Test flag")
 
 def main(args: argparse.Namespace):
     if args.predict is None:
         # We are training a model.
         np.random.seed(args.seed)
         train = Dataset()
+        
+        model = sklearn.pipeline.Pipeline([
+            ("TfidfVectorizer", sklearn.feature_extraction.text.TfidfVectorizer(strip_accents="unicode", max_features=20000, analyzer="char_wb", stop_words="english", ngram_range=(1,3))),
+            #("CountVectorizer", sklearn.feature_extraction.text.CountVectorizer(strip_accents="unicode", max_features=10000, analyzer="char_wb", stop_words="english", ngram_range=(1,3))),
+            #("HashingVectorizer", sklearn.feature_extraction.text.HashingVectorizer(strip_accents="unicode", analyzer="char_wb", ngram_range=(1,3), binary=True)),
+            ("MultinomialNB", sklearn.naive_bayes.MultinomialNB(alpha=0.5)),
+            #("LogisticRegression", sklearn.linear_model.LogisticRegression())
+        ])
 
-        # TODO: Train a model on the given dataset and store it in `model`.
-        model = None
+        if args.test:
+            scores = sklearn.model_selection.cross_val_score(model, train.data, train.target, scoring="f1", cv=5)
+            print("Cross-validation with 5 folds: {:.2f} +-{:.2f}".format(100 * scores.mean(), 100 * scores.std()))
+
+        # Train a model on the given dataset and store it in `model`.
+        model.fit(train.data, train.target)
 
         # Serialize the model.
         with lzma.open(args.model_path, "wb") as model_file:
@@ -56,9 +82,9 @@ def main(args: argparse.Namespace):
         with lzma.open(args.model_path, "rb") as model_file:
             model = pickle.load(model_file)
 
-        # TODO: Generate `predictions` with the test set predictions, either
+        # Generate `predictions` with the test set predictions, either
         # as a Python list or a NumPy array.
-        predictions = None
+        predictions = model.predict(test.data)
 
         return predictions
 
